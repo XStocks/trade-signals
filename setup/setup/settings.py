334472d06787d3b50c932fd9f731f5913ec23aa6
dtsuper3 <dtsuper3@gmail.com>
2022-05-29 13:22:51 +0530

    fx

--- setup/setup/settings.py
+++ setup/setup/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static/"),
